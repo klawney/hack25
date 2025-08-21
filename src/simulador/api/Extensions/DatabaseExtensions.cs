dc2c355102ed30e27567833ff13b44e25b8f3604
Claudinei da Costa <klawney@gmail.com>
2025-08-21 11:37:53 +0000

    Extensao para Database conexoes

---
+++ src/simulador/api/Extensions/DatabaseExtensions.cs
@@ -0,0 +1,29 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+
+namespace api.Extensions
+{
+    public static class DatabaseExtensions
+    {
+        // Extensão para Entity Framework com SQL Server
+        public static IServiceCollection AddMssqlEf<TContext>(this IServiceCollection services, IConfiguration config, string connectionName = "DefaultConnection") where TContext : DbContext
+        {
+            var connectionString = config.GetConnectionString(connectionName);
+            services.AddDbContext<TContext>(options =>
+                options.UseSqlServer(connectionString));
+            return services;
+        }
+
+        // Extensão para Dapper com SQL Server
+        public static IServiceCollection AddMssqlDapper(this IServiceCollection services, IConfiguration config, string connectionName = "DefaultConnection")
+        {
+            var connectionString = config.GetConnectionString(connectionName);
+            services.AddScoped<IDbConnection>(sp => new SqlConnection(connectionString));
+            return services;
+        }
+    }
+}
