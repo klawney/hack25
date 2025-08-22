c0744f425a32513bf72c1a7666a46abec70ebf12
Claudinei da Costa <klawney@gmail.com>
2025-08-22 03:34:29 +0000

    configuracao banco Sqlserver

--- src/simulador/api/Extensions/DatabaseExtensions.cs
+++ src/simulador/api/Extensions/DatabaseExtensions.cs
@@ -10,7 +10,7 @@ namespace api.Extensions
     public static class DatabaseExtensions
     {
         // Extensão para Entity Framework com SQL Server
-        public static IServiceCollection AddMssqlEf<TContext>(this IServiceCollection services, IConfiguration config, string connectionName = "DefaultConnection") where TContext : DbContext
+        public static IServiceCollection AddMssqlEf<TContext>(this IServiceCollection services, IConfiguration config, string connectionName = "SqlServerConnection") where TContext : DbContext
         {
             var connectionString = config.GetConnectionString(connectionName);
             services.AddDbContext<TContext>(options =>
@@ -19,7 +19,7 @@ namespace api.Extensions
         }
 
         // Extensão para Dapper com SQL Server
-        public static IServiceCollection AddMssqlDapper(this IServiceCollection services, IConfiguration config, string connectionName = "DefaultConnection")
+        public static IServiceCollection AddMssqlDapper(this IServiceCollection services, IConfiguration config, string connectionName = "SqlServerConnection")
         {
             var connectionString = config.GetConnectionString(connectionName);
             services.AddScoped<IDbConnection>(sp => new SqlConnection(connectionString));
